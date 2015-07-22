ff01b32115ec8f2a6cbf530f5ae82c24b5762f90
Christine Tran <tranchri@grinnell.edu>
2015-07-22 09:14:19 -0700

    Polished button toggling, styling, and changed/added title image

--- js/script.js
+++ js/script.js
@@ -3,17 +3,13 @@ $(document).ready (function() {
 		if($(".workText").is(":visible")) {
 			$(".workText").fadeToggle(300);
 		}
-		if($(".aboutText").is(":hidden")) {
-			$(".aboutText").fadeToggle(300);
-		}
+		$(".aboutText").fadeToggle(300);
 	});
 
 	$(".workButton").click(function() {
 		if($(".aboutText").is(":visible")) {
 			$(".aboutText").fadeToggle(300);
 		}
-		if($(".workText").is(":hidden")) {
-			$(".workText").fadeToggle(300);
-		}
+		$(".workText").fadeToggle(300);
 	});
 });
